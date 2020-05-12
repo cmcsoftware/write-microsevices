af7a640af451aabdfcebb8e612c2818848b27214
Marius Cristescu <fortech.cristescu@extaccount.com>
2020-05-12 13:58:12 +0300

    Add Jenkins file for pipeline.

--- src/main/java/com/microservice/write/test/WriteController.java
+++ src/main/java/com/microservice/write/test/WriteController.java
@@ -1,5 +1,7 @@
 package com.microservice.write.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,14 @@ import com.microservice.write.test.queue.QueueProducer;
 @RestController
 public class WriteController {
 
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	QueueProducer queueProducer;
 
 	@GetMapping("/write")
 	public ResponseEntity<Users> clear(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+		logger.info("Controller is called");
 		try {
 			Users user = new Users();
 			user.setName(name);
@@ -27,6 +32,12 @@ public class WriteController {
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		logger.info("Controller test is called");
+		return new ResponseEntity<>("Test endpoint called successfully", HttpStatus.OK);
 	}
 
 }
